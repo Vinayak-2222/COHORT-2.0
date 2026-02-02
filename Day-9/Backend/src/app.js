9fff442b55a6529d3f8f54172465dcbc40071374
Vinayak <officialvinayakst@gmail.com>
2026-02-02 16:23:04 +0530

    Day-9 complete

--- Day-9/Backend/src/app.js
+++ Day-9/Backend/src/app.js
@@ -3,9 +3,12 @@
 require('dotenv').config()
 const express=require("express")
 const noteModel=require("./models/note.model")
-const app =express()
-app.use(express.json())
+const cors=require("cors")
 
+const app =express()
+
+app.use(express.json())
+app.use(cors())
 //POST METHOD /api/notes   create a new note and store in mongodb  req.body={title,description}
 app.post("/api/notes",async(req,res)=>{
     const{title,description}=req.body
