6d450fa2e6f84df9aeab05a42c55196ec0b56ec1
Vinayak <officialvinayakst@gmail.com>
2026-02-09 19:43:14 +0530

    Authentication

--- Day-12/src/routes/auth.routes.js
+++ Day-12/src/routes/auth.routes.js
@@ -1,18 +1,21 @@
 const express=require("express")
 const usermodel=require("../models/user.model")
 const jwt=require("jsonwebtoken")
+const crypto=require("crypto")
 
 const authRouter=express.Router()  //we can create api other than app.js file
 authRouter.post("/register",async(req,res)=>{
     const {name,email,password}=req.body
     const isUserAlreadyExists = await usermodel.findOne({email})
     if(isUserAlreadyExists){
-        return res.status(400).json({
+        return res.status(409).json({
             message:"User already exists with this email address"
         })
     }
+
+    const hash=crypto.createHash("md5").update(password).digest("hex")
     const user=await usermodel.create({
-        name,email,password
+        name,email,password:hash
     })
     
     const token=jwt.sign(
@@ -31,5 +34,33 @@ authRouter.post("/register",async(req,res)=>{
     })
 })
 
+authRouter.post("/login",async(req,res)=>{
+    const {email,password}=req.body
+    const user=await usermodel.findOne({email})
+    if(!user){
+        return res.status(404).json({
+            message:"User not found with this email address"
+        })
+    }
+
+    const isPasswordMatched=user.password===crypto.createHash("md5").update(password).digest("hex")
+    if(!isPasswordMatched){
+        return res.status(401).json({
+            message:"Invalid Password"
+        })
+    }
+
+    const token=jwt.sign({
+        id:user._id,
+
+    },process.env.JWT_SECRET)
+
+    res.cookie("jwt_token",token)
+
+    res.status(200).json({
+        message:"User logged in successfully",
+        user
+    })
+})
 
 module.exports=authRouter
