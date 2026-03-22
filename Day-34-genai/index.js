524c6d3db6e69197b84fe8348782784015caa417
Vinayak <officialvinayakst@gmail.com>
2026-03-22 17:58:11 +0530

    genaitools

--- Day-34-genai/index.js
+++ Day-34-genai/index.js
@@ -1,7 +1,22 @@
 import "dotenv/config"
 import readline from "readline/promises"
 import { ChatMistralAI } from "@langchain/mistralai";
-import { HumanMessage } from "langchain";
+import { HumanMessage,tool,createAgent } from "langchain";
+import { sendEmail } from "./mail.service.js";
+import * as z from "zod"
+
+const emailTool=tool(
+    sendEmail,
+    {
+        name:"emailtool",
+        description:"use this tool to send an email",
+        schema:z.object({
+            to: z.string().describe("The recipient's email address"),
+            html: z.string().describe("The HTML content of the email"),
+            subject: z.string().describe("The subject of the email"),
+        })
+    }
+)
 
 const rl=readline.createInterface({
     input:process.stdin,
@@ -9,14 +24,23 @@ const rl=readline.createInterface({
 })
 const model=new ChatMistralAI({
     model:"mistral-small-latest"
+
+})
+
+const agent=createAgent({   //responsible to call email
+    model,
+    tools:[emailTool]
 })
 const messages=[]
 while(true){
     const userInput=await rl.question("You:")
     messages.push(new HumanMessage(userInput))
-    const response=await model.invoke(messages)
-    messages.push(response)
-    console.log("AI:" +response.text)
+    const response=await agent.invoke({
+        messages
+    })
+    messages.push(response.messages[ response.messages.length - 1 ])
+    //console.log("AI:" +response.text)
+    console.log(response.messages[ response.messages.length - 1 ].content)
 }
 
 
