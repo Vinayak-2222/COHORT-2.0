f84280ae29f10312bc76a710d65913d2ff9c0da6
Vinayak <officialvinayakst@gmail.com>
2026-06-08 23:38:59 +0530

    Sandbox-service

---
+++ kubernets/Backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    let sum = 0;
+
+    for (let i = 0; i < 1000000000; i++) {
+        sum += i;
+    }
+
+    res.send(`Hello WORLD! sum is ${sum}`);
+});
+
+const PORT=process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
