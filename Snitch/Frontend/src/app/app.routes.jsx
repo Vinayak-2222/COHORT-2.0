b46cbeda2a87fc61d17914208392f9d0287ecca6
Vinayak <officialvinayakst@gmail.com>
2026-07-04 13:36:41 +0530

    Snitch Day 3 - Product creation and dashboard

---
+++ Snitch/Frontend/src/app/app.routes.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router";
+import Register from "../features/auth/pages/Register";
+import Login from "../features/auth/pages/Login";
+import CreateProduct from "../features/products/pages/CreateProduct";
+import Dashboard from "../features/products/pages/Dashboard";
+import Protected from "../features/auth/components/Protected";
+
+export const routes= createBrowserRouter([
+    {
+        path:"/",
+        element:<h1>Hello world</h1>
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+   {
+        path: "/seller",
+        children: [
+            {
+                path:"/seller/create-product",
+                element:<Protected role="seller"><CreateProduct /></Protected>
+            },
+            {
+                path:"/seller/dashboard",
+                element:<Protected role="seller"><Dashboard /></Protected>
+            }
+        ]
+    }
+])
